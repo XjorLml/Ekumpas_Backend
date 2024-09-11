93d53f3442207621c0dc36dfd04ac845ad522761
XjorLml <lemnuellumaban@gmail.com>
2024-09-11 09:58:23 +0800

    added cors

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRouter = require('./routers/user_router');
 
 const app = express();
+app.use(cors());
 
-app.use(body_parser.json());
+app.use(bodyParser.json());
 app.use('/', userRouter);
 
 // Add this route to return "Backend server running" in the browser
 app.get('/', (req, res) => {
     res.send('Backend server running');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'Server is running' });
 });
 
 module.exports = app;
