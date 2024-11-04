d971ffcf9be369315a194291ed2026082b29b0f9
XjorLml <lemnuellumaban@gmail.com>
2024-11-04 11:47:05 +0800

    fixed all routes

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const UserController = require("../controller/user_controller");
 
-router.post("/register",UserController.register);
+router.post("/register", UserController.register);
 router.post('/verify-otp', UserController.verifyOTP);
-router.post("/login",UserController.login);
+router.post("/login", UserController.login);
 router.post('/forgot-password', UserController.requestPasswordReset);
 router.post('/verify-reset-otp', UserController.verifyPasswordResetOTP);
 router.post("/set-new-password", UserController.setNewPassword);
